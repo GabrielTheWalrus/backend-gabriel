d651b0f28e4af6ace26e31d72cbe3cacfdc52f9f
GabrielTheWalrus <gabriel.meale@hotmail.com>
2026-08-10 22:58:59 -0300

    refactor: switch to mocked database connection and enhance order processing endpoints

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.database.mocked import Mocked
 from app.database.postgresql import PostgresDbConnection
+from app.models.order import Order
 from app.service.store_service import StoreService
 
 import dotenv
@@ -35,7 +37,8 @@ app.add_middleware(
 )
 
 CONNECTION_STRING = f"postgresql://postgres.aeymuhifmzcadduyavjl:{os.getenv('db_pass')}@aws-0-sa-east-1.pooler.supabase.com:5432/postgres"
-POSTGRES_CONNECTION = PostgresDbConnection(CONNECTION_STRING)
+#POSTGRES_CONNECTION = PostgresDbConnection(CONNECTION_STRING)
+POSTGRES_CONNECTION = Mocked(CONNECTION_STRING)
 MERCH_SERVICE = StoreService(POSTGRES_CONNECTION)
 
 
@@ -46,3 +49,27 @@ def read_products():
 @app.get("/products/{merch_id}")
 def read_product(merch_id: int):
     return MERCH_SERVICE.get_product(merch_id)
+
+@app.post("/order")
+def create_order(order: Order):
+    if not MERCH_SERVICE.validate_order(order):
+        return {"status": "error", "message": "Invalid order data"}
+    
+    print(f"Received order: {order}")
+    return MERCH_SERVICE.create_order(order)
+
+@app.get("/shipping/{zipcode}")
+def calculate_shipping(zipcode: str):
+    shipping_price = MERCH_SERVICE.calculate_shipping(zipcode)
+    return {"zipcode": zipcode, "shipping_price": shipping_price}
+
+@app.post("/webhook/{order_id}/{payment_id}")
+def handle_payment_webhook(order_id: str, payment: str):
+    # Check if payment is "paid" or "failed" and update the order status accordingly
+    if payment == "paid":
+        # Update order status to "paid" in the database (mocked for now)
+        print(f"Order {order_id} has been paid.")
+    else:
+        # Update order status to "failed" in the database (mocked for now)
+        print(f"Payment for order {order_id} has failed.")
+    return {"message": f"Webhook received for payment status: {payment}"}
